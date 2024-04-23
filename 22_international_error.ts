8fd79afdbc20591acd82dd132f6d8240ca0df746
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:05:13 +0500

    Add files via upload

---
+++ 22_international_error.ts
@@ -0,0 +1,9 @@
+
+// creating array.
+let newArray : string [] = ['Bashir Ahmed', 'Taha Bashir', 'Hamza Bashir'] ;
+
+// Accessing array by index.
+console.log(newArray[5]) ;
+
+// correct array
+console.log(newArray);
