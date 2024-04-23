564363c5e4988d20e6f784fb82989ae36b971038
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:13:18 +0500

    Add files via upload

---
+++ 36_t_shirt.ts
@@ -0,0 +1,13 @@
+// call the function
+
+function make_shirt(size:string, text:string){
+
+    console.log(`Creat a ${size} shirt with the message: ${text}`) ;
+
+} 
+
+make_shirt('Large', 'Made in Pakistan') ;
+
+make_shirt('Mediam', 'Made in Pakistan') ;
+
+make_shirt('Small', 'Made in Pakistan') ;
