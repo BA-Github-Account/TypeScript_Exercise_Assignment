438f212d4c50e79270121dbfc58ab7aa1ac28137
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:15:53 +0500

    Add files via upload

---
+++ 41_magician.ts
@@ -0,0 +1,13 @@
+// Make an array
+
+let magician: string [] = ['Harry prtal', 'Hermione Granger', 'Ron Wasselly', 'Albus Dumbledore'];
+
+function show_magicians(magicians:string[]){
+
+magicians.forEach(element => {
+    console.log(element);
+
+});
+}
+
+show_magicians(magician);
