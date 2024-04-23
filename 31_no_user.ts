f2428539457956fdebff6c094a5564458663ccaf
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:10:03 +0500

    Add files via upload

---
+++ 31_no_user.ts
@@ -0,0 +1,8 @@
+let userNames: string [] = ['admin', 'Bashir', 'Hamza', 'Taha', 'Shajeeha']; 
+if(userNames.length === 0){
+    console.log('We need to some users!');
+}else{
+    userNames =[];
+    console.log('All user names have been removed. ' + userNames.length);
+}
+
