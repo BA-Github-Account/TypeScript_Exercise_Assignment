ef52caaa2222dc8c4fe0e54c3dab306a80bf9f2b
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:08:56 +0500

    Add files via upload

---
+++ 29_favorite_fruits.ts
@@ -0,0 +1,19 @@
+// Favorite fruits array of three fruits and lacc it favorite fruit
+
+let favorite_fruits: string []= ['Apple', 'banana', 'orange']  ;
+
+if(favorite_fruits.includes ('Apple')){
+    console.log('Ireally like apple');
+}
+if(favorite_fruits.includes ('banana')){
+    console.log('Ireally like banana');
+}
+if(favorite_fruits.includes ('mango')){
+    console.log('Ireally like mango');
+}
+if(favorite_fruits.includes ('stabri')){
+    console.log('Ireally like stabri');
+}
+if(favorite_fruits.includes ('orange')){
+    console.log('Ireally like orange');
+}
