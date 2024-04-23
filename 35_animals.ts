2c1e91eaa9db94f51446f21fe46e1d493fe8cd09
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:12:06 +0500

    Add files via upload

---
+++ 35_animals.ts
@@ -0,0 +1,9 @@
+// Crreating Array
+let petAnimals: string[]=['Cat', 'dog', 'rabit'];
+// Using for loop
+for (let petAnimal of petAnimals){
+    console.log(`A ${petAnimal} would make a great pet.`);
+}
+
+// A message outside of for loop
+console.log('Any of these animals would make a great pet!');
