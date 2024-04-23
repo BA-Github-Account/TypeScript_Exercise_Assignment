3fe690ea1ab82f9d23f17b3529e32d619d732daa
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:11:40 +0500

    Add files via upload

---
+++ 34_pizzas.ts
@@ -0,0 +1,12 @@
+// creating array
+let pizzas: string [] = ['pepperoni', 'cuban', 'chiken'];
+
+// using for loop
+
+for(let pizza of pizzas){
+    console.log(`I like ${pizza} pizza`);
+}
+
+// statement outside for loop
+
+console.log('I really love pizza') ;
