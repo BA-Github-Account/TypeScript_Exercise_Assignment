54aa2b286ac4a55e25e74228faf969e2cd432d0f
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 17:59:29 +0500

    Add files via upload

---
+++ 13_You_Own_Array.ts
@@ -0,0 +1,8 @@
+let transportation : string[] = ['car', 'motorcycle', 'cycle', 'train'] ;
+
+for(let i=0; i<transportation.length; i++) {
+
+ console.log('I would like to own a ' + transportation[i]) ;   
+}
+
+    
