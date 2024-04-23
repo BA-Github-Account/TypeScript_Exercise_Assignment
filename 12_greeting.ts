bdd67a649baea7ae9ea130b4a218306ac2d210c0
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 17:58:50 +0500

    Add files via upload

---
+++ 12_greeting.ts
@@ -0,0 +1,9 @@
+let names : string [] = ['Bashir', 'Taha', 'Hamza', 'Awaos'] ;
+
+const message : string = 'Welcme to TypeScript class: '
+
+for(let i=0; i<names.length; i++){
+    
+    console.log(message + names[i]) ;
+
+}
