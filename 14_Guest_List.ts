4977a13bda006b7a8c1b0f9bbd4b89b1c74e7fe9
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 17:59:56 +0500

    Add files via upload

---
+++ 14_Guest_List.ts
@@ -0,0 +1,5 @@
+let guest_list : string [] = ['Nawaz Shareef', 'Asif Ali Zardari', 'Imran khan'] ;
+for(let i=0; i<guest_list.length; i++){
+    console.log('Dear Mr. ' + guest_list[i] + ',\n\nit our pleasure to invite you in our party.\n\nTank You!') ;
+    
+}
