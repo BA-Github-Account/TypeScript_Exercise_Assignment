9ec22deac952ea9e5ba715d86c9a88a11211a9a7
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:03:48 +0500

    Add files via upload

---
+++ 20_Empty_Array.ts
@@ -0,0 +1,13 @@
+
+// Think something can be storein array
+let item : string[] = ['Pakistan', 'Islamabad', 'Urdu'] ;
+
+// Programme that creat  a list that containing these things.
+
+let item2 : string[] = [] ;
+
+item2.push('Saudi Arabia') ;
+item2.push('Canda') ;
+item2.push('United State of America');
+
+console.log(item2) ;
