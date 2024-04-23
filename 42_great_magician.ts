ef3d50a0d37412120a98d670a7cf8d716c5c9fbd
Bashir Ahmed <161707315+BA-Github-Account@users.noreply.github.com>
2024-04-23 18:16:32 +0500

    Add files via upload

---
+++ 42_great_magician.ts
@@ -0,0 +1,19 @@
+// Make an array of Great magicians
+
+let magician: string [] = ['Harry prtal', 'Hermione Granger', 'Ron Wasselly', 'Albus Dumbledore'];
+
+function make_great (magicianArray:string[]){
+
+    for(let i=0; i<magicianArray.length; i++){
+        magician[i] = ' The Great ' + magicianArray[i]
+    }
+}
+function show_magicians(magicians:string[]){
+
+magicians.forEach(element => {
+    console.log(element);
+
+});
+}
+make_great(magician);
+show_magicians(magician);
